de1e687af47fd0e337627c26a3e5504eb2d95532
Caregor <caregor@gmail.com>
2022-10-28 12:24:52 +0300

    Task 1 is done

---
+++ 1.js
@@ -0,0 +1,4 @@
+let tmp = prompt("Enter temperature in celsius");
+var Celsius = Number(tmp);
+Fahrenheit = (9/5) * Celsius + 32;
+alert("Temperature in celsius is " + Celsius + " temperature in fahrenheit is " + Fahrenheit.toFixed(1));
